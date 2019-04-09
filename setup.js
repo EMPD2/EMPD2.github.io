f2c531c1f3b0bbf91484ed051373244efe470408
Chilipp <p.sommer@posteo.de>
2019-04-10 00:17:34 +0200

    added climate diagram

--- setup.js
+++ setup.js
@@ -100,7 +100,7 @@ $(document).ready(function() {
       d.Edited = false;
       d.Temperature = $.map(d.Temperature.split(","), v => parseFloat(v) || NaN);
       d.Precipitation = d.Precipitation.split(",").map(
-          (v, i) => i < 12 ? parseFloat(v) : (i < 17 ? parseFloat(v) / 3 : parseFloat(v) / 12) || NaN);
+          (v, i) => i < 12 ? parseFloat(v) : (i < 16 ? parseFloat(v) / 3 : parseFloat(v) / 12) || NaN);
       d.ispercent = d.ispercent.toLowerCase().startsWith('f') ? false : true;
 
     // Limit latitudes according to latitude map range (-85:85)
@@ -205,20 +205,25 @@ $(document).ready(function() {
         d3.tsv(repo_url + 'samples/' + data[Id].SampleName + '.tsv').then(function(taxa_data) {
 
             blockMetaTable = true;
-            $('#meta-tabs a[href="#pollen-plot"]').tab('show');
+            activeTab = $('#climate-plot').hasClass("active") ? "climate-plot" : "pollen-plot";
 
             document.getElementById("pollen-diagram").innerHTML = "<svg/>";
             document.getElementById("pollen-diagram-legend").innerHTML = "<svg/>";
+            document.getElementById("climate-diagram").innerHTML = "<svg/>";
+            document.getElementById("climate-diagram-legend").innerHTML = "<svg/>";
             // document.getElementById("map-row").style.height = "800px";
+            $('#meta-tabs a[href="#pollen-plot"]').tab('show');
             plotPollen(taxa_data.filter(d => d.make_percent.toLowerCase() == "true"), "pollen-diagram");
             plotPollenLegend('pollen-diagram-legend');
+            $('#meta-tabs a[href="#climate-plot"]').tab('show');
+            plotClimate(data[Id], "climate-diagram");
+            plotClimateLegend("climate-diagram-legend");
+            $('#meta-tabs a[href="#' + activeTab + '"]').tab('show');
         });
 
     });
     theMap.on('popupclose', function(event) {
         Id = event.popup._source.key[2] - 1;
-        document.getElementById("pollen-diagram").innerHTML = "";
-        document.getElementById("pollen-diagram-legend").innerHTML = "";
         if (editor.root.collapsed == false) {
             editor.root.toggle_button.click();
         }
@@ -553,7 +558,7 @@ function highlightDisplayed() {
 function plotPollen(data, elemId) {
 
     var svg = d3.select("#" + elemId).select("svg"),
-        margin = {top: 40, right: 80, bottom: 180, left: 40},
+        margin = {top: 60, right: 80, bottom: 180, left: 40},
         width = 1000 - margin.left - margin.right,
         height = 300 - margin.top - margin.bottom;
 
@@ -584,6 +589,13 @@ function plotPollen(data, elemId) {
 
     var g = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var title = g.append("text")
+        .attr("dx", (width / 2))
+        .attr("y", -margin.top+20)
+        .attr("text-anchor", "middle")
+        .attr("class", "title")
+        .text(data[0].samplename);
 
     var maxPollen = Math.max.apply(Math, data.map(d => +d.percentage));
 
@@ -661,7 +673,7 @@ function plotPollen(data, elemId) {
       .data(groups)
       .enter().append("rect")
         .attr("class", "bar")
-        .attr("y", function(d) { return -margin.top+10; })
+        .attr("y", d => -margin.top+30)
         .attr("width", function(d, i) { return (d.count) * barWidth; })
         .attr("height", 10)
         .style("fill", function(d) { return groupColors[d.key] || Unkown_color; })
@@ -701,6 +713,176 @@ function plotPollenLegend(elemId) {
             .style("stroke", i == groups.length-1 ? groupColors[groups[0]] : "none")
             .style("stroke-dasharray", i == groups.length-1 ? ("10,3") : "none");
     })
+
+    var lbbox = g.node().getBBox();
+    g.append("rect")
+        .attr("x",(lbbox.x-legendPadding))
+        .attr("y",(lbbox.y-legendPadding))
+        .attr("height",(lbbox.height+2*legendPadding))
+        .attr("width",(lbbox.width+2*legendPadding))
+        .style("fill", "none")
+        .style("stroke", "black");
+}
+
+// ==================================================================
+function plotClimate(data, elemId) {
+    //graph code
+
+    var precip = data.Precipitation,
+        temperature = data.Temperature,
+        sampleName = data.SampleName;
+
+    var svg = d3.select("#" + elemId).select("svg"),
+        margin = {top: 40, right: 80, bottom: 180, left: 40},
+        width = 1000 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+    svg
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 960 240")
+
+    var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html((d, i) => "<table class='tooltip-table'>" +
+                      `<tr><td>Precipitation:</td><td>${Math.round(precip[i]*100)/100} mm/month</td></tr>` +
+                      `<tr><td>Temperature:</td><td>${Math.round(temperature[i]*100)/100} ºC</td></tr>` +
+                      `</table>`
+                  );
+
+    svg.call(tip);
+
+    var nbars = precip.length;
+    var barWidth = width / nbars;
+    var barPadding = 4;
+
+    var x = d3.scaleOrdinal().range(Array.from(Array(nbars).keys()).map(function(d) {return d * barWidth;})),
+        y = d3.scaleLinear().rangeRound([height, 0]),
+        y2 = d3.scaleLinear().rangeRound([height, 0]);
+
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var title = g.append("text")
+        .attr("dx", (width / 2))
+        .attr("y", -margin.top+20)
+        .attr("text-anchor", "middle")
+        .attr("class", "title")
+        .text(sampleName);
+
+    var maxTemp = Math.max.apply(Math, temperature);
+    var minTemp = Math.min.apply(Math, temperature);
+    var maxPrecip = Math.max.apply(Math, precip);
+
+    x.domain(monthsSeasons);
+    y.domain([minTemp, maxTemp]);
+    y2.domain([0, maxPrecip])
+
+    var temperatureLine = d3.line()
+        .x(function(d, i) {return x(i) + barWidth / 2})
+        .y(function(d, i) {return y(d)})
+
+    var xAxis = g => g
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(" + (barWidth / 2) +  "," + height + ")")
+        .call(d3.axisBottom(x).tickSizeOuter(0))
+        .selectAll("text")
+            .style("text-anchor", "middle");
+
+    var yAxis = g => g
+        .attr("class", "axis axis--y")
+        // .attr("transform", "translate(0" + width + " ,0)")
+        .call(d3.axisLeft(y).ticks(5))
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -30)
+        .attr("x", -40)
+        .attr("text-anchor", "middle")
+        .text("Temperature [ºC]");
+
+    var yAxis2 = g => g
+        .attr("class", "axis axis--y")
+        .attr("transform", "translate(" + width + " ,0)")
+        .call(d3.axisRight(y2).ticks(5))
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 40)
+        .attr("x", -30)
+        .attr("text-anchor", "middle")
+        .text("Precipitation [mm/month]");
+
+    g.append("g").call(xAxis);
+    g.append("g").call(yAxis);
+    g.append("g").call(yAxis2);
+
+    g.selectAll(".bar")
+      .data(precip)
+      .enter().append("rect")
+        .attr("class", "bar")
+        // .attr("x", function(d) { return x(d); })
+        .attr("y", d => y2(d))
+        .attr("width", barWidth - barPadding)
+        .attr("height", d => height - y2(d))
+        .style("fill", "steelblue")
+        .attr("transform", function (d, i) {
+             var translate = [barWidth * i + barPadding / 2 , 0];
+             return "translate("+ translate +")";})
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
+
+    g.append("path")        // Add the temperature path.
+        .style("stroke", "FireBrick")
+        .style("fill", "none")
+        .attr('stroke-width', 2)
+        .attr("d", temperatureLine(temperature))
+        .attr("transform", function (d, i) {
+             var translate = [barWidth * i + barPadding , 0];
+             return "translate("+ translate +")";
+        });
+
+    // vertical line to separate months and seasons
+    g.append("line")
+        .attr("x1", x(12))
+        .attr("y1", 0)
+        .attr("x2", x(12))
+        .attr("y2", 300 - margin.top - margin.bottom)
+        .style("stroke-dasharray", ("3, 3"))
+        .style("stroke-width", 2)
+        .style("stroke", "black")
+        .style("fill", "none");
+
+}
+
+function plotClimateLegend(elemId) {
+    var svg = d3.select("#" + elemId).select("svg");
+
+    legendPadding = 10;
+
+    var g = svg.append("g").attr("class", "legend")
+            .style("font-size", "18px")
+            .attr("transform", "translate(25, 40)");
+
+    g.append("text")
+        .attr("y", 0)
+        .attr("x", "1em")
+        .text("Precipitation");
+    g.append("circle")
+        .attr("cy", -0.25+"em")
+        .attr("cx", 0)
+        .attr("r", "0.4em")
+        .style("fill", "steelblue");
+
+    g.append("text")
+        .attr("y", "1em")
+        .attr("x", "1em")
+        .text("Temperature");
+    g.append("line")
+        .attr("x1", 0)
+        .attr("x2", "1em")
+        .attr("y1", "0.75em")
+        .attr("y2", "0.75em")
+        .style("stroke", "FireBrick")
+        .attr('stroke-width', 2);
 
     var lbbox = g.node().getBBox();
     g.append("rect")
